ee9e39dca779153fcd18f113270805e2a170bd52
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-28 17:06:26 +0530

    created search bar and fetch results from tvMaze api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is the Home Page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onSearch = (search = input) => {
+    // https://api.tvmaze.com/search/shows?q=action
+    const url = `https://api.tvmaze.com/search/shows?q=${search}`;
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const KeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      {' '}
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={KeyDown}
+        value={input}
+      />
+      <br />
+      <button
+        type="button"
+        onClick={() => {
+          onSearch();
+        }}
+      >
+        {' '}
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
