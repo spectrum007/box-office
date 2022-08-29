50d14eb01069aa59cb72a8cc6bc623effb7dc291
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-29 15:13:25 +0530

    intalled styled components and added style to search cards

--- src/Components/Actors/ActorGrid.js
+++ src/Components/Actors/ActorGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ActorCards from './ActorCards';
 import ImageNotFound from '../../Images/not-found.png';
+import { FlexGrid } from '../Styled';
 
 const ActorGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({ person }) => (
         <ActorCards
           key={person.id}
@@ -17,7 +18,7 @@ const ActorGrid = ({ data }) => {
           summary={person.summary}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
